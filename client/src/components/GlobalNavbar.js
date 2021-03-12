8ce40f7dd0a0a1e6aa9299b3c28212992e28c1d9
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-03-12 15:28:20 -0800

    deleted unnecessary files, updated nav bar

--- client/src/components/GlobalNavbar.js
+++ client/src/components/GlobalNavbar.js
@@ -6,12 +6,10 @@ function GlobalNavbar() {
         <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#home">Google Books</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Search</Nav.Link>
                     <Nav.Link href="/saved">Saved</Nav.Link>
                 </Nav>
-            </Navbar.Collapse>
         </Navbar>
     )
 }
