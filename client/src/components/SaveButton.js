db2324d0d53435f76fb1f5a47aaed25ba165021e
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-03-16 13:31:35 -0700

    updated savebutton post api route

--- client/src/components/SaveButton.js
+++ client/src/components/SaveButton.js
@@ -1,69 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { store } from 'react-notifications-component';
 import 'animate.css/animate.css'
 import API from "../utils/api"
 ;
 function SaveButton({result}) {
-    //Code for potential duplicating, currently unable to get it to work.
-
-    // const [savedBook, setSavedBook] = useState([]);
-    // const idArray = [];
-    // const emptyArray = [];
-
-    // useEffect(() => {
-    //     loadBooks()
-    //   }, [])
-
-    
-    // const handleIdArray = () => {
-    //     for (var i = 0; i < savedBook.length; i++) {  
-    //         idArray.push(savedBook[i].id)
-    //         }
-    //         console.log(idArray)
-    //         console.log(savedBook)
-    // }
-
-    // const clearIdArray = () => {
-    //     idArray.length = 0;
-    //     console.log(idArray);
-    // }
-
-    // const loadBooks = (req,res) => {
-    //     API.getBooks(res)
-    //         .then(res => {
-    //             setSavedBook(res.data);
-    //             console.log(res.data);      
-    //         })
-    //         .catch(err => console.log(err));
-    // }
-
-    //create a find one api? to compare 1 id
 
     const handleSave = event => {
         event.preventDefault();
-        // handleIdArray()
-
-        // conditional for empty array
-        // if (savedBook == emptyArray) {
-        //     API.saveBook({
-        //         id:   result.id,  
-        //         title: result.volumeInfo.title,
-        //         authors: result.volumeInfo.authors,
-        //         description: result.volumeInfo.description,
-        //         image: result.volumeInfo.imageLinks.thumbnail,
-        //         link: result.volumeInfo.previewLink
-        //     })
-        //     .then(res => 
-        //     handleSavedNotification(),
-        //     loadBooks()
-        //     ).catch(err => console.log(err))
-        // } else {
-        
-            // for (var i = 0; i < idArray.length; i++) {
-            //         console.log(idArray[i]);
-            //         console.log(result.id);
-            //         (idArray[i] !== result.id)
                 if (result.id) {
                     API.saveBook({
                         id:   result.id,  
@@ -75,17 +19,9 @@ function SaveButton({result}) {
                     })
                     .then(res => 
                         handleSavedNotification()
-                        // clearIdArray(),
-                        // loadBooks()
                         ).catch(err => console.log(err))
                 } 
-                // else {
-                //     handleErrorNotification()
-                //     return;
-                // }
             }
-        // }
-    // }
 
         const handleSavedNotification = () => {
             store.addNotification({
@@ -105,25 +41,6 @@ function SaveButton({result}) {
                 }
             });
         }
-
-        // const handleErrorNotification = () => {
-        //     store.addNotification({
-        //         title: "ERROR",
-        //         message: result.volumeInfo.title + " has already been saved",
-        //         type: "danger",
-        //         insert: "top",
-        //         container: "top-right",
-        //         animationIn: ["animate__animated", "animate__fadeIn"],
-        //         animationOut: ["animate__animated", "animate__fadeOut"],
-        //         dismiss: {
-        //             duration: 2000,
-        //             onScreen: true,
-        //             pauseOnHover: true,
-        //             click: true,
-        //             touch: true
-        //         }
-        //     });
-        // }
 
     return (
         <>
